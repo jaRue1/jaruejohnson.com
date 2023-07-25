37922d613a1c23b1087481f0a75989dca7e6ae2f
jaRue1 <jaruejohnson@outlook.com>
2023-07-25 18:52:08 -0400

    first commit

---
+++ public/assets/index.ts
@@ -0,0 +1,6 @@
+// import logo from './images/logo.png';
+// import profileImg from './images/profile.png';
+
+// export { logo, profileImg };
+
+// also remember to add .ico file for logo in public/assets folder
